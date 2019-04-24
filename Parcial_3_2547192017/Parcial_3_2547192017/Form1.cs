ae5cac2c8eca6491a4c75617e46659e9663d382d
ahenriquez94 <47991678+ahenriquez94@users.noreply.github.com>
2019-04-23 20:04:02 -0600

    Add files via upload

---
+++ Parcial_3_2547192017/Parcial_3_2547192017/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Parcial_3_2547192017
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String usuario, passwd;
+            usuario = textBox1.Text.TrimEnd();
+            passwd = textBox2.Text.TrimEnd();
+
+            if((usuario=="amilcar")&(passwd=="programacion")){
+                MessageBox.Show("Bienvenido al Sistema");
+                MDIParent1 obj1 = new MDIParent1();
+                obj1.Show();
+            }
+            else
+            {
+                MessageBox.Show("USUARIO O CONTRASEÑA INVALIDA");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
---
+++ Parcial_3_2547192017/Parcial_3_2547192017/Form2.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Parcial_3_2547192017
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double cant, tanto, total, totalsum;
+
+            if(radioButton1.Checked==true){
+                cant = double.Parse(textBox1.Text);
+                tanto = double.Parse(textBox2.Text);
+
+                total = (cant / 100) * tanto;
+
+                textBox3.Text = total.ToString();
+
+                totalsum = total + cant;
+
+                textBox4.Text = totalsum.ToString();
+
+            }else if(radioButton2.Checked==true){
+
+                cant = double.Parse(textBox1.Text);
+                tanto = double.Parse(textBox2.Text);
+
+                total = (cant / 100) * tanto;
+
+                textBox3.Text = total.ToString();
+
+                textBox4.Text = cant.ToString();
+            }
+
+           
+
+        }
+    }
+}
